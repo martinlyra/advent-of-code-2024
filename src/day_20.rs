dfb10bbcd126b8a989d18b8baf6107c0aa96d727
Martin Lyrå <martin.lyra.97@gmail.com>
2024-12-20 14:56:17 +0100

    Finish day 20

--- src/day_20.rs
+++ src/day_20.rs
@@ -2,8 +2,6 @@ mod day_16;
 
 use std::collections::HashMap;
 
-use cached::SizedCache;
-use cached::proc_macro::cached;
 use day_16::{find_paths, read_input, Path};
 use itertools::Itertools;
 use rayon::prelude::*;
@@ -12,57 +10,56 @@ fn main() {
     let test = read_input("./input/day_20.test.txt");
     let input = read_input("./input/day_20.txt");
 
+    // Challenge unit-tests
     let test_paths = find_paths(&test);
     let test_path = test_paths.iter().next().unwrap();
 
-    let map = part_1(&test_path);
+    let map = part_1(&test_path, 0);
     println!("First part test answer:",);
     for key in map.keys().sorted() {
         println!("- {key}: {}", map[key]);
     }
 
+    let map = part_2(&test_path, 50);
+    println!("Second part test answer:",);
+    for key in map.keys().sorted() {
+        println!("- {key}: {}", map[key]);
+    }
+
+    // Challenge input
     let input_paths = find_paths(&input);
     let input_path = input_paths.iter().next().unwrap();
-    let map = part_1(&input_path);
+    let map = part_1(&input_path, 100);
     println!(
         "First part answer: {}",
-        map.iter()
-            .filter_map(|(k, v)| {
-                if *k >= 100 {
-                    return Some(v);
-                }
-                None
-            })
-            .sum::<i32>()
-    )
+        map.values().sum::<i32>()
+    );
+    let map = part_2(&input_path, 100);
+    println!(
+        "Second part answer: {}",
+        map.values().sum::<i32>()
+    );
 }
 
-#[cached(
-    ty = "SizedCache<String, Option<usize>>",
-    create = "{ SizedCache::with_size(256) }",
-    convert = r#"{ format!("{:p}{}{}", path, position.0, position.1) }"#,
-)]
-fn find_index_of(path: &Vec<(usize, usize)>, position: (usize, usize)) -> Option<usize> {
-    path.iter().position(|&p| p == position)
+fn manhattan(a: &(usize, usize), b: &(usize, usize)) -> usize {
+    a.0.abs_diff(b.0) + a.1.abs_diff(b.1)
 }
 
-fn find_shortcuts(path: &Path) -> Vec<(usize, usize)> {
-    path.visited
-        .par_iter()
-        .enumerate()
-        .map(|(i, &p)| {
-            let tail = path.visited[i..].to_vec();
-            let directions = vec![(1, 0), (0, 1), (-1, 0), (0, -1)];
-
-            directions
-                .iter()
-                .filter_map(|(dx, dy)| {
-                    let (x, y) = (
-                        ((p.0 as i32) + (2 * dx)) as usize,
-                        ((p.1 as i32) + (2 * dy)) as usize,
-                    );
-
-                    tail.iter().position(|&p| p == (x, y)).map(|j| (i, i + j))
+fn find_cheat_points(path: &Path, max_length: usize) -> Vec<(usize, usize, usize)> {
+    (0..path.visited.len())
+        .into_par_iter()
+        .map(|i| {
+            let p = path.visited[i];
+            let tail = (i + 1)..path.visited.len();
+            tail.into_iter()
+                .filter_map(move |j| {
+                    let o = path.visited[j];
+                    let l = manhattan(&p, &o);
+                    if l <= max_length {
+                        Some((i, j, l))
+                    } else {
+                        None
+                    }
                 })
                 .collect::<Vec<_>>()
         })
@@ -70,13 +67,23 @@ fn find_shortcuts(path: &Path) -> Vec<(usize, usize)> {
         .collect()
 }
 
-fn part_1(path: &Path) -> HashMap<usize, i32> {
-    find_shortcuts(path)
+fn eval(path: &Path, max_length: usize, minimum: usize) -> HashMap<usize, i32> {
+    find_cheat_points(path, max_length)
+        .par_iter()
+        .map(|(i, j, l)| j - i - l)
+        .filter(|&x| x >= minimum)
+        .collect::<Vec<_>>()
         .iter()
-        .map(|(i, j)| j - i - 2)
-        .filter(|&x| x > 0)
-        .fold(HashMap::new(), |mut map, f| {
+        .fold(HashMap::new(), |mut map, &f| {
             map.entry(f).and_modify(|c| *c += 1).or_insert(1);
             map
         })
+}
+
+fn part_1(path: &Path, minimum: usize) -> HashMap<usize, i32> {
+    eval(path, 2, minimum)
+}
+
+fn part_2(path: &Path, minimum: usize) -> HashMap<usize, i32> {
+    eval(path, 20, minimum)
 }
