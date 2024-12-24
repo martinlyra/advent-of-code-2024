28842c5982bd0997878bf71a64657dcce12e8978
Martin Lyrå <martin.lyra.97@gmail.com>
2024-12-25 00:21:20 +0100

    Attempt at optimize day 23 I guess

--- src/day_23.rs
+++ src/day_23.rs
@@ -1,50 +1,51 @@
 use std::{
-    collections::{HashMap, HashSet, VecDeque},
+    collections::{HashMap, HashSet},
     fs::read_to_string,
+    sync::Arc,
 };
 
 use itertools::Itertools;
 use regex::Regex;
 
-struct Graph {
-    nodes: HashSet<String>,
-    edges: HashMap<String, HashSet<String>>,
+struct Graph<T> {
+    nodes: HashSet<Arc<T>>,
+    edges: HashMap<Arc<T>, HashSet<Arc<T>>>,
 }
 
-impl Graph {
-    fn add_edge(&mut self, node_a: &str, node_b: &str) {
-        let a = String::from(node_a);
-        let b = String::from(node_b);
-        self._add_to_edges(a.clone(), b.clone());
-        self._add_to_edges(b.clone(), a.clone());
+impl<T: std::cmp::Eq + std::hash::Hash + ToOwned> Graph<T> {
+    fn add_edge(&mut self, node_a: T, node_b: T) {
+        let a = Arc::new(node_a);
+        let b = Arc::new(node_b);
+        self._add_to_edges(&a, &b);
+        self._add_to_edges(&b, &a);
         self.nodes.insert(a);
         self.nodes.insert(b);
     }
 
-    fn _add_to_edges(&mut self, node_a: String, node_b: String) {
+    fn _add_to_edges(&mut self, node_a: &Arc<T>, node_b: &Arc<T>) {
         self.edges
-            .entry(node_a)
+            .entry(node_a.clone())
             .or_insert(HashSet::new())
-            .insert(node_b);
+            .insert(node_b.clone());
     }
 
-    fn find_cliques(&self) -> Vec<HashSet<String>> {
-        self._bron_kerbosch(HashSet::new(), self.nodes.clone(), HashSet::new())
+    fn find_maximal_cliques(&self) -> Vec<HashSet<Arc<T>>> {
+        self._bron_kerbosch_1(HashSet::new(), self.nodes.clone(), HashSet::new())
     }
 
-    fn _bron_kerbosch(
+    fn _bron_kerbosch_1(
         &self,
-        r: HashSet<String>,
-        mut p: HashSet<String>,
-        mut x: HashSet<String>,
-    ) -> Vec<HashSet<String>> {
+        r: HashSet<Arc<T>>,
+        mut p: HashSet<Arc<T>>,
+        mut x: HashSet<Arc<T>>,
+    ) -> Vec<HashSet<Arc<T>>> {
         if p.is_empty() && x.is_empty() {
             return vec![r];
         }
         let mut cliques = Vec::new();
         for v in p.clone() {
             let neighbours = &self.edges[&v];
-            cliques.extend(self._bron_kerbosch(
+            cliques.extend(self._bron_kerbosch_1(
                 r.union(&HashSet::from([v.clone()])).cloned().collect(),
                 p.clone().intersection(neighbours).cloned().collect(),
                 x.intersection(neighbours).cloned().collect(),
@@ -58,7 +59,7 @@ impl Graph {
 
 fn main() {
     let test = read_input("./input/day_23.test.txt");
-    let input: Graph = read_input("./input/day_23.txt");
+    let input = read_input("./input/day_23.txt");
 
     println!("First part test answer: {} == 7", part_1(&test));
     println!("First part answer: {}", part_1(&input));
@@ -67,7 +68,7 @@ fn main() {
     println!("Second part answer: {}", part_2(&input));
 }
 
-fn part_1(graph: &Graph) -> usize {
+fn part_1(graph: &Graph<String>) -> usize {
     graph
         .nodes
         .iter()
@@ -96,9 +97,9 @@ fn part_1(graph: &Graph) -> usize {
         .count()
 }
 
-fn part_2(graph: &Graph) -> String {
+fn part_2(graph: &Graph<String>) -> String {
     graph
-        .find_cliques()
+        .find_maximal_cliques()
         .iter()
         .sorted_by(|a, b| Ord::cmp(&a.len(), &b.len()))
         .rev()
@@ -109,7 +110,7 @@ fn part_2(graph: &Graph) -> String {
         .join(",")
 }
 
-fn read_input(file_path: &str) -> Graph {
+fn read_input(file_path: &str) -> Graph<String> {
     let edge_pattern = Regex::new(r"(\w+)-(\w+)").unwrap();
     read_to_string(file_path)
         .unwrap()
@@ -126,7 +127,7 @@ fn read_input(file_path: &str) -> Graph {
                 edges: HashMap::new(),
             },
             |mut graph, edge| {
-                graph.add_edge(edge.0, edge.1);
+                graph.add_edge(edge.0.to_owned(), edge.1.to_owned());
                 graph
             },
         )
